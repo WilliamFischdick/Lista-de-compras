9676f9ada0aa1723f6a6d690016a4173367cbab9
William Fischdick <jwfbit@hotmail.com>
2023-04-09 20:23:43 -0300

    :construction: feat: Criando o armazenamento de dados no localStorage.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,47 @@
 
 const form = document.getElementById("novoItem");
 const lista = document.getElementById("lista");
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
+
+itens.forEach((elemento) => {
+    criaElemento(elemento)
+});
+
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault();
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+
+    const nome = evento.target.elements['nome'];
+    const quantidade = evento.target.elements['quantidade'];
+   
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value
+    }
+
+    criaElemento(itemAtual);
+
+    itens.push(itemAtual);
+
+    localStorage.setItem("itens", JSON.stringify(itens));
+
+    nome.value = "";
+    quantidade.value = "";
 })
 
-function criaElemento(nome, quantidade) {
-    const novoItem = document.createElement('li');
+function criaElemento(item) {
     
-    novoItem.classList.add("item");
-
+    const novoItem = document.createElement('li');
     const numeroItem = document.createElement('strong');
     
-    numeroItem.innerHTML = quantidade;
-
+    novoItem.classList.add("item");
+    
+    numeroItem.innerHTML = item.quantidade;
+    
     novoItem.appendChild(numeroItem); 
-    novoItem.innerHTML += nome;
-
+   
+    novoItem.innerHTML += item.nome;
+   
     lista.appendChild(novoItem);
 
 }
